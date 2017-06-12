7be7222c8147f605b5b86b3961154efce7209b7d
zeratulmdq <colomdq85@hotmail.com>
2017-06-12 12:44:31 -0300

    Readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# Ejemplos WebRTC
+
+[Demo en vivo](https://utn-mdp.github.io/webrtc/)
+
+### Para iniciarlo:
+
+```bash
+# clonen el proyecto:
+git clone https://github.com/UTN-MDP/webrtc.git
+
+# entren en la carpeta
+cd webrtc
+
+# inicien un servidor web
+php -S localhost:8888
+
+# si no tienen PHP instalado, abran el archivo index.html directamente en el navegador.
+```
--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 'use strict';
 
+// Referencia a los botones
 const video = document.getElementById('video');
 const qvga = document.getElementById('qvga');
 const vga = document.getElementById('vga');
 const hd = document.getElementById('hd');
 const fullHd = document.getElementById('fullhd');
 
+// Agregamos listeners al evento click
 qvga.addEventListener('click', function() {
     play({video: {width: 320, height: 240}});
 });
@@ -22,12 +24,11 @@ fullHd.addEventListener('click', function() {
     play({width: 1920, height: 1080});
 });
 
+// Empezamos a reproducir el video
 function play(constraints) {
     if (video.srcObject)
-    {
         //video.srcObject.getTracks().forEach(track => track.stop());
         video.srcObject = null;
-    }
 
     navigator.mediaDevices.getUserMedia(constraints)
         .then(mediaStream => video.srcObject = mediaStream)
